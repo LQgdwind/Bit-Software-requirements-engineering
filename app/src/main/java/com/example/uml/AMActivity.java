ad4634d2939cee41a294d2d197300b68de095162
LQgdwind <lqgdwind@foxmail.com>
2022-04-17 21:31:43 +0800

    4.17 third

--- app/src/main/java/com/example/uml/AMActivity.java
+++ app/src/main/java/com/example/uml/AMActivity.java
@@ -1,8 +1,8 @@
 package com.example.uml;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class AMActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/uml/MainActivity.java
+++ app/src/main/java/com/example/uml/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.uml;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
