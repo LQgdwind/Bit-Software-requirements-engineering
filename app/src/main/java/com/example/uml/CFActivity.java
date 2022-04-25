c7645d0a4b3dc4b025ff1c8be82f6e2b174a3768
dyhes <1325574784@qq.com>
2022-04-25 22:21:05 +0800

    lsj_merged

--- app/src/main/java/com/example/uml/CFActivity.java
+++ app/src/main/java/com/example/uml/CFActivity.java
@@ -1,14 +1,74 @@
+
 package com.example.uml;
 
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.InputFilter;
+import android.widget.EditText;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager2.widget.ViewPager2;
+import com.example.uml.Fragments.Fragments_All;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 public class CFActivity extends AppCompatActivity {
 
+    //ViewPager2 Components
+    ViewPager2 mViewPager;
+
+    // TabLayout Components
+    private FragmentAdapter mFragmentAdapter;
+    private TabLayout tabLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cf);
+        //Initializing
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        mViewPager = (ViewPager2) findViewById(R.id.viewPager);
+
+        //Binding TabLayout and ViewPager2
+        FragmentManager fm = getSupportFragmentManager();
+
+        mViewPager.setAdapter(mFragmentAdapter);
+
+        //Set TabLayout
+        //binding ViewPager2 and Fragments
+        tabLayout.addTab(tabLayout.newTab().setText("全部"));
+        tabLayout.addTab(tabLayout.newTab().setText("表白"));
+        tabLayout.addTab(tabLayout.newTab().setText("吐槽"));
+        tabLayout.addTab(tabLayout.newTab().setText("知乎"));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                mViewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        mViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+        mFragmentAdapter = new FragmentAdapter(fm, getLifecycle());
+        mViewPager.setAdapter(mFragmentAdapter);
     }
 }
--- app/src/main/java/com/example/uml/SAActivity.java
+++ app/src/main/java/com/example/uml/SAActivity.java
@@ -2,7 +2,10 @@ package com.example.uml;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class SAActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class SAActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sa);
+        String[] userInfo=getIntent().getStringArrayExtra("userInfo");
+        Log.e("am",userInfo[0]+" "+userInfo[1]+" "+userInfo[2]);
+        findViewById(R.id.leaveRequest).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Start(LeaveActivity.class,userInfo);
+            }
+        });
+        findViewById(R.id.appointment).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Start(ReservationActivity.class,userInfo);
+            }
+        });
+    }
+    public void Start(Class<?> activityClass, String[] userInfo){
+        Intent intent=new Intent(this,activityClass);
+        intent.putExtra("userInfo",userInfo);
+        startActivity(intent);
     }
 }
