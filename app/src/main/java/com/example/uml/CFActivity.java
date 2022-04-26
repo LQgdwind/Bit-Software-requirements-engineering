a6ac793d4033a0c522a95ab5b0a6605b58a06cbb
dyhes <1325574784@qq.com>
2022-04-26 14:17:11 +0800

    fake_diamond

--- app/src/main/java/com/example/uml/CFActivity.java
+++ app/src/main/java/com/example/uml/CFActivity.java
@@ -4,6 +4,8 @@ package com.example.uml;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.InputFilter;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
@@ -17,6 +19,8 @@ import com.google.android.material.tabs.TabLayoutMediator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class CFActivity extends AppCompatActivity {
 
@@ -70,5 +74,39 @@ public class CFActivity extends AppCompatActivity {
         });
         mFragmentAdapter = new FragmentAdapter(fm, getLifecycle());
         mViewPager.setAdapter(mFragmentAdapter);
+
+        //set publish button
+        Button publishButton = (Button) findViewById(R.id.cf_publish);
+        String[] userinfo = getIntent().getStringArrayExtra("userInfo");
+
+        publishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                String commentInfoText = "  #4 " + userinfo[2] + "|普通                                                                         刚刚";
+                TextView commentInfo = (TextView) findViewById(R.id.AllLayoutWindow1_top);
+                commentInfo.setText(commentInfoText);
+
+                EditText input = (EditText) findViewById(R.id.cfEditText);
+                String text = "        " + input.getText().toString();
+                TextView mainText = (TextView) findViewById(R.id.AllLayoutWindow1_main);
+                mainText.setText(text);
+
+                RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.AllLayoutWindow1);
+                relativeLayout.setVisibility(View.VISIBLE);
+
+                input.setText("");
+
+                AlertDialog.Builder dialog=new AlertDialog.Builder(CFActivity.this);
+                dialog.setTitle("BIT_BBS");
+                dialog.setMessage("发表成功!");
+                dialog.setCancelable(false);
+                dialog.setNegativeButton("确认", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+                dialog.show();
+            }
+        });
     }
 }
